d075531eebe2c565eff07c7a94f34253e0aad462
Martina Galabova <martina.galabova@sap.com>
2018-10-06 11:57:25 +0300

    add diagnostics

--- cmd/go-sofia/main.go
+++ cmd/go-sofia/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/martinagalabova/go-sofia/internal/diagnostics"
 )
 
 func main() {
 	log.Print("Hello Go Sofia!")
 
-	err := http.ListenAndServe(":8080", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, http.StatusText(http.StatusOK))
+	})
+
+	go func() {
+		err := http.ListenAndServe(":8080", router)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	diagnostics := diagnostics.NewDiagnostics()
+	err := http.ListenAndServe(":8585", diagnostics)
 	if err != nil {
 		log.Fatal(err)
 	}
